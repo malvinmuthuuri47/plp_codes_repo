6875eba9efe4692560ec11ffbbf0ccf3b86b5b0e
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2025-08-14 00:07:48 +0300

    Make the solution file dynamic in that it accepts different files specified by the user but otherwise falls back to the default files in the directory. Make the uppercase file dynamic too in that it either accepts a new file from the command line and checks it or it falls back to the default file

--- python_folder/week_four/check_uppercase.py
+++ python_folder/week_four/check_uppercase.py
@@ -1,7 +1,18 @@
-with open("output.txt", "r") as f:
-	text = f.read()
+import sys
+
+# get output filename from command line, default to 'output.txt'
+if len(sys.argv) > 1:
+    output_file = sys.argv[1]
+else:
+    output_file = "output.txt"
+
+try:
+    with open("output.txt", "r") as f:
+	    text = f.read()
 	
-	if text.isupper():
-		print("The file content is uppercase")
-	else:
-		print("The file content is lowercase")
+	    if text.isupper():
+		    print(f"The file {output_file}'s content is uppercase")
+	    else:
+		    print("The file {output_file}'s content is lowercase")
+except Exception as e:
+    print(f"Unexpected error: {e}")
--- python_folder/week_four/solution.py
+++ python_folder/week_four/solution.py
@@ -1,7 +1,14 @@
+# ask the user for file names
+program_input_file = input("Enter the input file name (default: input.txt): ") or "input.txt"
+program_output_file = input("Enter the output file name (default: output.txt): ") or "output.txt"
+
+if not program_output_file.lower().endswith(".txt"):
+    program_output_file += ".txt"
+
 word_count = 0
 
 try:
-    with open("input.txt", "r") as f, open("output.txt", "w") as out:
+    with open(program_input_file, "r") as f, open(program_output_file, "w") as out:
         for line in f:
             words = line.split()
             word_count += len(words)
@@ -9,6 +16,6 @@ try:
             processed_line = line.upper()
             out.write(processed_line)
 
-    print(f"Done! Created 'output.txt' in the current directory.")
+    print(f"Done! Created {program_output_file} in the current directory.")
 except Exception as e:
-    print(f"Unexpected error {e}")
+    print(f"Unexpected error: {e}")
